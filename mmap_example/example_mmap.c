2a83315a1109be1d2c690262d078571884dcc08d
Abhishek Dubey <abhishek.dubey@vanderbilt.edu>
2016-11-10 15:21:13 -0600

    Update example_mmap.c

--- mmap_example/example_mmap.c
+++ mmap_example/example_mmap.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>     /* exit, EXIT_FAILURE */
 #include <stdio.h>
 
+//Find out the eize of the file
 size_t sizeFileFunc(const char* filename) {
     struct stat st;
     stat(filename, &st);
@@ -14,7 +15,7 @@ size_t sizeFileFunc(const char* filename) {
 }
 
 int main(int argc, char** argv) {
-     
+     //argv[1] is the name of the file to be read using mmap
     if(argv[1] == NULL){
     	printf("Need to supply the file name to be read!!\r\n");
         exit(EXIT_FAILURE); 
@@ -23,11 +24,13 @@ int main(int argc, char** argv) {
     //Open file
     int fd = open(argv[1], O_RDONLY, 0);
     assert(fd != -1);
-    //Execute mmap
+    //Execute mmap . Note that the alternative would have been to use read instead of mmap.
     void* data = mmap(NULL, fsize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
     assert(data != NULL);
+    
     //Write the mmapped data to stdout (= FD #1)
     write(1, data, fsize);
+    
     //Cleanup
     int rc = munmap(data, fsize);
     assert(rc == 0);
