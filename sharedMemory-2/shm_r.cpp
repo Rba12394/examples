d621b569b38f3919e7be11cfa0bb59e4bac50645
abhishek <abhishek.dubey@vanderbilt.edu>
2016-11-10 22:57:30 +0100

    another shared memory example

--- sharedMemory-2/shm_r.cpp
+++ sharedMemory-2/shm_r.cpp
@@ -1,50 +1,73 @@
+#include <sys/mman.h>
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <mqueue.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <errno.h>
 #include <iostream>
+
+
+struct mymessage
+{
+    int i;
+};
+
 
 int main (int argc, char **argv)
 {
-//  mqd_t mq = mq_open ("/MyCoolMQ", O_RDONLY | O_CREAT);
+  mymessage *shm_segment = 0;
+  int shm_fd (-1);
 
-  struct mq_attr config;
-  config.mq_flags = 0;
-  config.mq_maxmsg = 3;
-  config.mq_msgsize = 5;
-  config.mq_curmsgs = 0;
+  int ret=0;
 
-  mqd_t mq = mq_open ("/MyCoolMQ", O_RDONLY |O_CREAT, S_IRUSR | S_IWUSR |
-                                                   S_IRGRP | S_IROTH,&config);
+  // Open the shared memory segment. Note that it has been opened in Read
+  // write mode
+  shm_fd = shm_open ("/SHM_example",
+                     O_CREAT  | O_RDWR,
+                     S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
-  if (mq == -1)
+  if (shm_fd < 0)
     {
-      perror ("mq_open");
-      return -1;
+      perror ("shm_open");
+      ret = -1;
+      goto exit;
     }
 
-  struct mq_attr attr;
-  mq_getattr (mq, &attr);
-
-  char *buf = new char[attr.mq_maxmsg];
-
-  for ( ;; )
+  // Set the size of the shared memory segment
+  if (ftruncate (shm_fd, sizeof (mymessage)) != 0)
     {
-      unsigned int prio;
-      ssize_t received = mq_receive (mq, buf, attr.mq_msgsize, &prio);
-
-      if (received == -1)
-        {
-          perror ("mq_receive");
-          return -1;
-        }
-
-      if (prio == 24) break;
-
-      std::cerr << "Received " << received << " bytes at " << prio << "priority: " << buf << std::endl;
+      perror ("ftruncate");
+      ret = -1;
+      goto exit;
     }
-  mq_close(mq);
-  mq_unlink ("/MyCoolMQ");
 
+  // Map the shared memory segment into memory
+
+  // The PROT_READ you to only read from this segment
+  // shared memory segment. This is the page level access that mmap controls.
+
+  // the map shared
+  if ((shm_segment =
+           (mymessage *) mmap (0, sizeof (mymessage), (PROT_READ ),
+                               MAP_SHARED, shm_fd, 0)) == MAP_FAILED)
+    {
+      perror ("mmap");
+      ret = -1;
+      return ret;
+    }
+
+
+
+  for (size_t i = 0; i < 25; ++i)
+    {
+
+      std::cerr << "Recevied message " << shm_segment->i << std::endl;
+
+      sleep (1);
+    }
+
+  exit:
+  shm_unlink ("/SHM_example");
+  *(reinterpret_cast < int *>(shm_segment)) = 0;
+  return ret;
 }
--- sharedMemory-2/shm_s.cpp
+++ sharedMemory-2/shm_s.cpp
@@ -1,3 +1,4 @@
+#include <sys/mman.h>
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <mqueue.h>
@@ -5,31 +6,69 @@
 #include <stdio.h>
 #include <iostream>
 
+
+struct mymessage
+{
+    int i;
+};
+
+
 int main (int argc, char **argv)
 {
-  struct mq_attr config;
-  config.mq_flags = 0;
-  config.mq_maxmsg = 3;
-  config.mq_msgsize = 5;
-  config.mq_curmsgs = 0;
+  mymessage *shm_segment = 0;
+  int shm_fd (-1);
 
-  mqd_t mq = mq_open ("/MyCoolMQ", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, &config);
+  int ret=0;
 
-  if (mq == -1)
+  // Open the shared memory segment. Note that it has been opened in Read
+  // write mode
+  shm_fd = shm_open ("/SHM_example",
+                     O_CREAT | O_EXCL | O_RDWR,
+                     S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+  if (shm_fd < 0)
     {
-      perror ("mq_open");
-      return -1;
+      perror ("shm_open");
+      ret = -1;
+      goto exit;
     }
+
+  // Set the size of the shared memory segment
+  if (ftruncate (shm_fd, sizeof (mymessage)) != 0)
+    {
+      perror ("ftruncate");
+      ret = -1;
+      goto exit;
+    }
+
+  // Map the shared memory segment into memory
+
+  // The PROT_READ and PROT_WRITE Allows you to both read and write from the
+  // shared memory segment. This is the page level access that mmap controls.
+
+  // the map shared
+  if ((shm_segment =
+           (mymessage *) mmap (0, sizeof (mymessage), (PROT_READ | PROT_WRITE),
+                               MAP_SHARED, shm_fd, 0)) == MAP_FAILED)
+    {
+      perror ("mmap");
+      ret = -1;
+     return ret;
+    }
+
+
 
   for (size_t i = 0; i < 25; ++i)
     {
-      char buf[5];
-      int count = snprintf (buf, 5, "%lu ", i);
-      std::cerr << "Sending message " << buf << std::endl;
-      mq_send (mq, buf, count, i);
+
+      std::cerr << "Sending message " << i << std::endl;
+      shm_segment->i=i;
 
       sleep (1);
     }
-  mq_close(mq);
-  mq_unlink ("/MyCoolMQ");
+
+  exit:
+    shm_unlink ("/SHM_example");
+    *(reinterpret_cast < int *>(shm_segment)) = 0;
+  return ret;
 }
